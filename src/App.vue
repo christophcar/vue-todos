67a9f77c3949e05cfd0f7d95743ad2b450aadaa5
Christoph <carstens.christoph@gmail.com>
2019-02-10 21:21:36 +0100

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -4,39 +4,39 @@
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </nav>
-
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Skills from './components/Skills.vue'
+import Skills from "./components/Skills.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Skills
   }
-}
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
 
 body {
-  background-color: #EEEEEE;
-  font-family: 'Montserrat', sans-serif;
+  background-color: #eeeeee;
+  font-family: "Montserrat", sans-serif;
   display: grid;
   grid-template-rows: auto;
   justify-items: center;
   padding-top: 50px;
 }
-body, html {
+body,
+html {
   margin: 0;
   height: 100%;
 }
 #app {
-    width: 50%;
+  width: 50%;
 }
 
 nav {
